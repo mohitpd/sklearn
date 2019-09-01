c5089b868909dca7c0c4c6ee4771291c2d54593f
mohitpd <mohitprasad28@gmail.com>
2019-09-01 20:33:34 +0530

    Breast Cancer Classification

---
+++ br_cancer.py
@@ -0,0 +1,19 @@
+from sklearn import preprocessing, neighbors
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('data/breast-cancer-wisconsin.data')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+
+X = np.array(df.drop(['class'],1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
